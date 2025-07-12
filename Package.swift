54c0dbfd2c882d52e94f2bc0f3100b001ab9eb38
Isaac Halvorson <hello@hisaac.net>
2025-07-12 15:04:17 -0500

    A small start

--- Package.swift
+++ Package.swift
@@ -6,12 +6,14 @@ let package = Package(
 	name: "odo",
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.6.1"),
+		.package(url: "https://github.com/jpsim/Yams.git", from: "5.1.3"),
 	],
 	targets: [
 		.executableTarget(
 			name: "odo",
 			dependencies: [
 				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "Yams", package: "Yams"),
 			]
 		),
 	]
--- Sources/odo/odo.swift
+++ Sources/odo/odo.swift
@@ -1,8 +1,22 @@
 import ArgumentParser
+import Foundation
 
 @main
 struct odo: ParsableCommand {
+	static let configuration = CommandConfiguration(
+		abstract: "A CLI tool for managing system configuration from YAML files"
+	)
+
+	@Argument(help: "Path to the YAML configuration file")
+	var configPath: String
+
+	@Flag(name: .shortAndLong, help: "Show verbose output")
+	var verbose = false
+
+	@Flag(name: .shortAndLong, help: "Show what would be changed without making changes")
+	var dryRun = false
+
 	mutating func run() throws {
-		print("Hello, world!")
+
 	}
 }
